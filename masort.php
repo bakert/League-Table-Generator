e39f3d5e3d28a52b04e72fd872412a022d069636
Thomas David Baker <bakert@gmail.com>
2011-04-03 21:38:07 -0700

    Clean up double concatenation of masort.

--- masort.php
+++ masort.php
@@ -73,23 +73,6 @@ function get_comparison_function($key, $data) {
     return 'strcasecmp';
 }
 
-/* test data
-$data = array(
-    array('A' => 'lemon',
-          'B' => 'chicken'),
-    array('A' => 'orange',
-          'B' => 'duck'),
-    array('A' => 'lemon',
-          'B' => 'sherbert'),
-    array('A' => 'orange',
-          'B' => 'juice')
-);
-
-print "<pre>";
-masort("A_a,B_a", $data);
-print_r($data);
-*/
-
 function numcmp($a, $b) {
     if ($a > $b) {
         return 1;
@@ -98,51 +81,6 @@ function numcmp($a, $b) {
     } else {
         return 0;
     }
-}
-
-?>
-23:18:26 bluebone@base:~$ cat public_html/masort.txt
-<?php
-
-// Expects Multidimensional associative array for the first param.
-// Second param is "field_a,field_d,field_a" as first param - _a for sort
-// ascending, _d for sort descending.
-function masort(&$data, $sort) {
-    if (!$sort || !$data) {
-        return $data;
-    }
-    $function = create_sort_function($sort, $data);
-    return uasort($data, $function);
-}
-
-function create_sort_function($sort, $data) {
-    $f = '';
-    foreach (explode(",", $sort) as $raw) {
-        $ending = substr($raw, -strlen("_d"), strlen("_d"));
-        if ($ending !== '_a' && $ending !== '_d') {
-            $ending = '';
-        }
-        $key  = substr($raw, 0, strlen($raw) - strlen($ending));
-        $desc = ($ending === "_d");
-        $cmp  = get_comparison_function($key, $data);
-        $f .= '$res = ' . $cmp . '($a["' . $key . '"], $b["' . $key . '"]); '
-            . 'if ($res != 0) { '
-                . 'return ' . ($desc ? '-$res' : '$res') . '; '
-            . '} ';
-    }
-    $f .= 'return $a;';
-    return create_function('$a, $b', $f);
-}
-
-//Look at the data and guess what the best comparator is.
-function get_comparison_function($key, $data) {
-    foreach ($data as $row) {
-        $value = $row[$key];
-        if (is_numeric($value)) {
-            return 'numcmp';
-        }
-    }
-    return 'strcasecmp';
 }
 
 /* test data
@@ -161,16 +99,6 @@ print "<pre>";
 masort("A_a,B_a", $data);
 print_r($data);
 */
-
-function numcmp($a, $b) {
-    if ($a > $b) {
-        return 1;
-    } elseif ($b > $a) {
-        return -1;
-    } else {
-        return 0;
-    }
-}
 
 /*
 Copyright (c) 2004 Thomas David Baker
